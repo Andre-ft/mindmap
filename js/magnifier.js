69b2d49073c74db443a1f8773594d10251cc3869
Andrey F <media.andrej@gmail.com>
2021-03-24 17:21:36 +0200

    js

--- js/magnifier.js
+++ js/magnifier.js
@@ -36,7 +36,7 @@ function magnify(imgID, zoom) {
     if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
     if (x < w / zoom) {x = w / zoom;}
     if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
-    if (y < h / zoom +50) {y = h / zoom +50;}
+    if (y < h / zoom +100) {y = h / zoom +100;}
     /*set the position of the magnifier glass:*/
     glass.style.left = (x - w) + "px";
     glass.style.top = (y - h) + "px";
